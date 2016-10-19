2f9104c0002b81766501d4776f253bdbe52a79e5
seans <seans@Sean-Laptop>
2016-10-19 20:41:02 +0100

    Fixed so that errors are removed in the doGet method of the Login Servlet

--- src/main/java/uk/ac/dundee/computing/aec/instagrim/servlets/Login.java
+++ src/main/java/uk/ac/dundee/computing/aec/instagrim/servlets/Login.java
@@ -108,6 +108,7 @@ public class Login extends HttpServlet {
         
     }
     protected void doGet(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+        HttpSession session=request.getSession();
         session.setAttribute("LoginError", "");
         RequestDispatcher rd=request.getRequestDispatcher("login.jsp");
 	rd.forward(request,response);
